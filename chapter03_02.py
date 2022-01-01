3f914fc15812b7935cbebbbc15341fc06af791f2
SangYong-Jeong <mmaf@naver.com>
2022-01-01 22:11:51 +0900

    파이썬 복습

--- chapter03_02.py
+++ chapter03_02.py
@@ -43,6 +43,75 @@ print(t_s2)
 print()
 
 # Raw String 출력 -> escape를 사용하지 않는 방법(escape 무시) 
-raw_s = r'D:\python\test'
+raw_s = r"D:\python\test"
 
 print(raw_s)
+
+# 멀티라인 입력
+# 역슬래시 사용 (문자열 표현 방식에서 줄바꿈을 막는 역할)
+multi_str = \
+"""
+문자열 
+멀티라인 입력  
+테스트
+"""
+
+print(multi_str)
+
+
+# 문자열 연산
+str_o1 = 'Python'
+str_o2 = 'Apple'
+str_o3 = 'How are you doing'
+str_o4 = 'Seoul Daejeon Busan Jinju'
+
+print(str_o1 * 3)
+print(str_o1 + str_o2)
+print('y' in str_o1) # y라는 알파벳이 포함되어있니? (시퀀스형이 쓸 수 있음) 
+print('z' in str_o1) 
+print('P' not in str_o2)
+
+# 문자열 형 변환
+print(str(66), type(str(66)))
+print(str(10.1))
+print(str(True), type(str(True)))
+
+# 문자열 함수(upper, isalnum, startswith, count, endswith, isalpha...)
+# 추가적인 부분 예제 파일 확인
+print("Capitalize: ", str_o1.capitalize())
+print("endswith?: ", str_o2.endswith("!"))
+print("replace", str_o1.replace("thon", " Good"))
+print("sorted: ", sorted(str_o1))
+print("split: ", str_o4.split(' '))
+
+# 반복(시퀀스)
+im_str = "Good Boy!"
+
+print(dir(im_str)) # __iter__ 가 있으면 반복 가능
+
+# 출력
+for i in im_str:
+  print(i)
+
+
+# 슬라이싱
+str_s1 = "Nice Python"
+
+# 슬라이싱 연습
+print(str_s1[0:3]) # 0 1 2 인덱스의 str 이 합쳐져서 나온다.
+print(str_s1[5:]) # [5:11] 같다(5부터 끝까지 가져와)
+print(str_s1[:len(str_s1)]) # str_sl[:11] (첫부분도 생략가능 0임)
+print(str_s1[:len(str_s1) - 1]) # str_sl[:10] (첫부분도 생략가능 0임)
+print(str_s1[:]) # str_sl[:11]
+print(str_s1[1:9:2]) # 2칸 씩 뛰어서 가져와라
+print(str_s1[-5:])
+print(str_s1[1: -2])
+print(str_s1[::2]) # 처음 부터 끝까지 2칸 간격으로 가져와라
+print(str_s1[::-1]) # 음수는 오른쪽에서 왼쪽으로
+
+# 아스키 코드(또는 유니코드)
+a = 'z'
+
+print(ord(a)) # 아스키 코드로
+print(chr(122)) # 문자로
+
--- 복습/chapter03_02.py
+++ 복습/chapter03_02.py
@@ -57,3 +57,81 @@ print()
 raw_S = r'D:\python\test'
 
 print(raw_S)
+
+# 멀티라인 출력
+multi_str1 = \
+"""
+문자열 
+멀티라인 입력
+테스트
+"""
+print(multi_str1)
+
+# 멀티라인(역슬래시) 출력
+multi_str2 = \
+'''
+문자열 멀티라인
+역슬래시(\) \
+테스트
+'''
+print(multi_str2)
+
+# 문자열 연산
+str_o1 = 'Python'
+str_o2 = 'Apple'
+str_o3 = 'How are you doing?'
+str_o4 = 'Seoul Daejeon Busan Jinju'
+
+print(3 * str_o1) # 문자열 * int형 -> 문자열 int형 값 만큼 반복
+print(str_o1 + str_o2) # 문자열 + 문자열 -> str
+print(dir(str_o1)) # __iter__ (반복 가능 속성)
+print('y' in str_o1) # y가 str_o1에 있니? return bool
+print('n' in str_o1) 
+print('P' not in str_o2)
+
+# 문자열 형 변환
+print(type(str(66))) # type int -> str
+print(str(10.1), type(str(10.1)))
+print(str(True), type(str(True)))
+print(str(complex(12)), type(str(complex(12))))
+
+# 문자열 함수 (upper, isalnum, startswith, count, endswith, isalpha 등 ...)
+print("Capitalize: ", str_o1.capitalize()) # 첫 문자 대문자로
+print("endswith?: ", str_o2.endswith("s"))
+print("join str: ", str_o1.join(["I'm", "!"]))
+print("replace1: ", str_o1.replace("are", "was"))
+print("replace2: ", str_o3.replace("are", "was"))
+print("split: ", str_o4.split(' '))
+print("sorted: ", sorted(str_o1))
+print("reversed1: ", reversed(str_o2)) #list 형 변환
+print("reversed2: ", list(reversed(str_o2)))
+
+# 반복 (시퀀스) 설명
+im_str = "Good Boy!"
+
+print(dir(im_str)) # __iter__ 확인
+
+# 출력
+for i in im_str:
+  print(i)
+
+# 슬라이싱
+str_sl = 'Nice Python'
+
+# 슬라이싱 연습
+print(str_sl[0:3]) # 뒤에 숫자 -1 까지 
+print(str_sl[:len(str_sl)])
+print(str_sl[:len(str_sl) - 1])
+print(str_sl[:])
+print(str_sl[1:4:2])
+print(str_sl[-4:-2])
+print(str_sl[1:-2])
+print(str_sl[::-1])
+print(str_sl[::2])
+
+# 아스키코드
+a = 'z'
+
+print(ord(a))
+print(chr(122))
+
